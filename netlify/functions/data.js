5d04c8685df0dc592f849d9ed1f51d9e27108c26
Aldana <aldannab@gmail.com>
2025-12-05 12:01:17 -0300

    Persistencia real con Netlify Blobs

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -1,21 +1,23 @@
-let DATA = {
-    participantes: [],
-    misiones: []
-};
+import { getStore } from "@netlify/blobs";
 
 export async function handler(event) {
+    const store = getStore("amigo-invisible");
+
     if (event.httpMethod === "GET") {
+        const participantes = (await store.get("participantes")) || [];
+        const misiones = (await store.get("misiones")) || [];
+
         return {
             statusCode: 200,
-            body: JSON.stringify(DATA)
+            body: JSON.stringify({ participantes, misiones })
         };
     }
 
     if (event.httpMethod === "POST") {
         const body = JSON.parse(event.body);
 
-        if (body.participantes) DATA.participantes = body.participantes;
-        if (body.misiones) DATA.misiones = body.misiones;
+        if (body.participantes) await store.set("participantes", body.participantes);
+        if (body.misiones) await store.set("misiones", body.misiones);
 
         return {
             statusCode: 200,
